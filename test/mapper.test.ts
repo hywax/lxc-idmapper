6753baad99d528af9b7822887270486c0a2fc78d
hywax <me@hywax.space>
2024-03-12 15:33:38 +0500

    test: implements 3 tests

--- test/mapper.test.ts
+++ test/mapper.test.ts
@@ -1,19 +1,27 @@
 import { describe, expect, it } from 'vitest'
+import type { MapperMatch } from '../src'
 import { mapper } from '../src'
 
 describe('mapper', () => {
-  it.todo('correct generation of a single match', () => {
+  it('should handle an empty array', () => {
+    const matches: MapperMatch[] = []
+    const result = {
+      idMap: [],
+      subId: [],
+    }
+
+    expect(mapper(matches)).toEqual(result)
+  })
+
+  it('should generation of a single match', () => {
     const matches = [
       { containerId: 1000, hostId: 1005 },
     ]
     const result = {
       idMap: [
-        'u 0 100000 1000',
-        'g 0 100000 1000',
-        'u 1000 1005 1',
-        'g 1000 1005 1',
-        'u 1001 101001 64535',
-        'g 1001 101001 64535',
+        '0 100000 1000',
+        '1000 1005 1',
+        '1001 101001 64535',
       ],
       subId: [
         'root:1005:1',
@@ -23,23 +31,18 @@ describe('mapper', () => {
     expect(mapper(matches)).toEqual(result)
   })
 
-  it.todo('correct generation of multiple comparisons', () => {
+  it('should generation of multiple comparisons', () => {
     const matches = [
       { containerId: 44, hostId: 44 },
       { containerId: 107, hostId: 104 },
     ]
     const result = {
       idMap: [
-        'u 0 100000 44',
-        'g 0 100000 44',
-        'u 44 44 1',
-        'g 44 44 1',
-        'u 45 100045 62',
-        'g 45 100045 62',
-        'u 107 104 1',
-        'g 107 104 1',
-        'u 108 100108 65428',
-        'g 108 100108 65428',
+        '0 100000 44',
+        '44 44 1',
+        '45 100045 63',
+        '107 104 1',
+        '108 100108 65428',
       ],
       subId: [
         'root:44:1',
